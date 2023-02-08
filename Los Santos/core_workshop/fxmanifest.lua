f6c9a5a67018509d18acf130a4a07dcb697dc79d
Guudz <Kilian1702@outlook.com>
2023-02-08 07:03:07 +0100

    Initial commit

---
+++ Los Santos/core_workshop/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+    "interiorproxies.meta"
+}
